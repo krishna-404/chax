4ab6a98f57609976f48218dd172ee981224c0a12
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-15 21:04:49 +0530

    outputing list of rooms using functional component

--- lib/chax_web/live/chat_room_live.ex
+++ lib/chax_web/live/chat_room_live.ex
@@ -6,6 +6,24 @@ defmodule ChaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+      <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+        <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-lg font-bold text-gray-800">
+              Slax
+            </h1>
+          </div>
+        </div>
+        <div class="mt-4 overflow-auto">
+          <div class="flex items-center h-8 px-3 group">
+            <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+          </div>
+          <div id="rooms-list">
+            <%!-- A function component is any function that receives an assigns map as an argument and returns a rendered struct built with the ~H sigil --%>
+            <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+          </div>
+        </div>
+      </div>
       <div class="flex flex-col flex-grow shadow-lg">
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
@@ -21,6 +39,25 @@ defmodule ChaxWeb.ChatRoomLive do
     """
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        <%= @room.name %>
+      </span>
+    </a>
+    """
+  end
+
   def mount(_params, _session, socket) do
     IO.puts("mounting")
     if connected?(socket) do
@@ -28,10 +65,12 @@ defmodule ChaxWeb.ChatRoomLive do
     else
       IO.puts("mounting (not connected)")
     end
-    room = Room |> Repo.all() |> List.first()
+    rooms = Room |> Repo.all()
+    room = List.first(rooms)
 
     socket =
       socket
+      |> assign(:rooms, rooms)
       |> assign(:room, room)
       |> assign(:hide_topic, true)
 
