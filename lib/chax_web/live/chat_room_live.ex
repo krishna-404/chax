88f628bc7844dcdb2103cf6483ab72f77bf530c1
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-18 11:00:30 +0530

    Serving static image as avatar

--- lib/chax_web/live/chat_room_live.ex
+++ lib/chax_web/live/chat_room_live.ex
@@ -210,7 +210,7 @@ defmodule ChaxWeb.ChatRoomLive do
         >
           <.icon name="hero-trash" class="h-4 w-4" />
         </button>
-        <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
+        <img class="h-10 w-10 rounded flex-shrink-0" src={~p"/images/one_ring.jpg"} />
         <div class="ml-2">
           <div class="-mt-1">
             <.link class="text-sm font-semibold hover:underline">
