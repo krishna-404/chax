a3156e79f572f3d66d1167e55e52f6bfb2f4db76
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-18 16:29:26 +0530

    add date dividers

--- lib/chax_web/live/chat_room_live.ex
+++ lib/chax_web/live/chat_room_live.ex
@@ -139,18 +139,26 @@ defmodule ChaxWeb.ChatRoomLive do
         phx-update="stream"
       >
         <%= for {dom_id, message} <- @streams.messages do %>
-          <%= if message == :unread_marker do %>
-            <div id={dom_id} class="w-full flex text-red-500 items-center gap-3 pr-5">
-              <div class="w-full h-px grow bg-red-500"></div>
-              <div class="text-sm">New</div>
-            </div>
-          <% else %>
-            <.message
-              current_user={@current_user}
-              dom_id={dom_id}
-              message={message}
-              timezone={@timezone}
-            />
+          <%= case message do %>
+            <% :unread_marker -> %>
+              <div id={dom_id} class="w-full flex text-red-500 items-center gap-3 pr-5">
+                <div class="w-full h-px grow bg-red-500"></div>
+                <div class="text-sm">New</div>
+              </div>
+            <% %Message{} -> %>
+              <.message
+                current_user={@current_user}
+                dom_id={dom_id}
+                message={message}
+                timezone={@timezone}
+              />
+            <% %Date{} -> %>
+              <div id={dom_id} class="flex flex-col items-center mt-2">
+                <hr class="w-full" />
+                <span class="flex items-center justify-center -mt-3 bg-white h-6 px-3 rounded-full border text-xs font-semibold mx-auto">
+                  <%= format_date(message) %>
+                </span>
+              </div>
           <% end %>
         <% end %>
      </div>
@@ -212,6 +220,31 @@ defmodule ChaxWeb.ChatRoomLive do
        <.room_form form={@new_room_form} />
     </.modal>
     """
+  end
+
+  defp format_date(%Date{} = date) do
+    today = Date.utc_today()
+
+    case Date.diff(today, date) do
+      0 ->
+        "Today"
+
+      1 ->
+        "Yesterday"
+
+      _ ->
+        format_str = "%A, %B %e#{ordinal(date.day)}#{if today.year != date.year, do: " %Y"}"
+        Timex.format!(date, format_str, :strftime)
+    end
+  end
+
+  defp ordinal(day) do
+    cond do
+      rem(day, 10) == 1 and day != 11 -> "st"
+      rem(day, 10) == 2 and day != 12 -> "nd"
+      rem(day, 10) == 3 and day != 13 -> "rd"
+      true -> "th"
+    end
   end
 
   attr :dom_id, :string, required: true
@@ -346,6 +379,7 @@ defmodule ChaxWeb.ChatRoomLive do
       dom_id: fn
         %Message{id: id} -> "message-#{id}"
         :unread_marker -> "messages-unread-marker"
+        %Date{} = date -> to_string(date)
       end
     )
     |> ok()
@@ -368,7 +402,11 @@ defmodule ChaxWeb.ChatRoomLive do
 
     last_read_id = Chat.get_last_read_id(room, current_user)
 
-    messages = Chat.list_messages_in_room(room) |> maybe_insert_unread_marker(last_read_id)
+    messages =
+      room
+      |> Chat.list_messages_in_room()
+      |> insert_date_dividers(socket.assigns.timezone)
+      |> maybe_insert_unread_marker(last_read_id)
 
     Chat.update_last_read_id(room, current_user)
 
@@ -391,6 +429,19 @@ defmodule ChaxWeb.ChatRoomLive do
       |> noreply()
   end
 
+  defp insert_date_dividers(messages, nil), do: messages
+
+  defp insert_date_dividers(messages, timezone) do
+    messages
+    |> Enum.group_by(fn message ->
+      message.inserted_at
+      |> DateTime.shift_zone!(timezone)
+      |> DateTime.to_date()
+    end)
+    |> Enum.sort_by(fn {date, _msgs} -> date end, &(Date.compare(&1, &2) != :gt))
+    |> Enum.flat_map(fn {date, messages} -> [date | messages] end)
+  end
+
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
   end
@@ -398,7 +449,11 @@ defmodule ChaxWeb.ChatRoomLive do
   defp maybe_insert_unread_marker(messages, nil), do: messages
 
   defp maybe_insert_unread_marker(messages, last_read_id) do
-    {read, unread} = Enum.split_while(messages, &(&1.id <= last_read_id))
+    {read, unread} =
+      Enum.split_while(messages, fn
+        %Message{} = message -> message.id <= last_read_id
+        _ -> true
+      end)
 
     if unread == [] do
       read
