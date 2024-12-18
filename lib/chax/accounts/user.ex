8987f0c0e5b45dee28e4c5613dde8f6261b7470b
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-18 14:54:57 +0530

    backend changes to accept usernames

--- lib/chax/accounts/user.ex
+++ lib/chax/accounts/user.ex
@@ -41,8 +41,9 @@ defmodule Chax.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :username])
     |> validate_email(opts)
+    |> validate_username(opts)
     |> validate_password(opts)
   end
 
@@ -63,6 +64,16 @@ defmodule Chax.Accounts.User do
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
     |> maybe_hash_password(opts)
+  end
+
+  defp validate_username(changeset, opts) do
+    changeset
+    |> validate_required([:username])
+    |> validate_format(:username, ~r/^[A-Za-z0-9-]+$/,
+      message: "can only contain letters, numbers and dashes"
+    )
+    |> validate_length(:username, max: 20)
+    |> maybe_validate_unique_username(opts)
   end
 
   defp maybe_hash_password(changeset, opts) do
@@ -87,6 +98,16 @@ defmodule Chax.Accounts.User do
       changeset
       |> unsafe_validate_unique(:email, Chax.Repo)
       |> unique_constraint(:email)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_validate_unique_username(changeset, opts) do
+    if Keyword.get(opts, :validate_username, true) do
+      changeset
+      |> unsafe_validate_unique(:username, Slax.Repo)
+      |> unique_constraint(:username)
     else
       changeset
     end
