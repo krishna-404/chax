194953acb167c3149453f73d9ddc61af1600eee6
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-19 13:54:43 +0530

    view to handle the navbar visible

--- assets/js/hooks/RoomMessages.js
+++ assets/js/hooks/RoomMessages.js
@@ -5,31 +5,33 @@ init({ data })
 
 const RoomMessages = {
   mounted() {
-    this.el.scrollTo({
-      top: this.el.scrollHeight,
-      behavior: "smooth",
-    });
+    setTimeout(() => {
+      this.scrollToBottom();
+    }, 100);
 
     this.handleEvent("scroll_messages_to_bottom", () => {
-      this.el.scrollTo({
-        top: this.el.scrollHeight,
-        behavior: "smooth",
-      });
+      this.scrollToBottom();
     });
+
+    this.scrollToBottom = () => {
+      requestAnimationFrame(() => {
+        this.el.scrollTop = this.el.scrollHeight;
+      });
+    };
 
     this.canLoadMore = true;
 
     this.el.addEventListener("scroll", e => {
       if (this.canLoadMore && this.el.scrollTop < 100) {
         this.canLoadMore = false;
-         const prevHeight = this.el.scrollHeight;
+        const prevHeight = this.el.scrollHeight;
 
-         this.pushEvent("load-more-messages", {}, (reply) => {
-           this.el.scrollTo(0, this.el.scrollHeight - prevHeight);
-           this.canLoadMore = reply.can_load_more;
-         });
-       }
-    })
+        this.pushEvent("load-more-messages", {}, (reply) => {
+          this.el.scrollTo(0, this.el.scrollHeight - prevHeight);
+          this.canLoadMore = reply.can_load_more;
+        });
+      }
+    });
 
     this.handleEvent("reset_pagination", ({can_load_more}) => {
       this.canLoadMore = can_load_more;
